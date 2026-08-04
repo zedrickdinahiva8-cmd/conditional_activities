ac9dc51156f9c535e2382a417780962084aa87c5
dreik_dnhv <zedrickdinahiva8@gmail.com>
2026-08-04 15:07:20 +0800

    Create If-Else_Statement.cs

---
+++ If-Else_Statement.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter an integer: ");
+        int value = Convert.ToInt32(Console.ReadLine());
+
+        if (value >= 0)
+        {
+            Console.WriteLine("The integer " + value + " is positive.");
+        }
+        else
+        {
+            Console.WriteLine("The integer " + value + " is negative.");
+        }
+    }
+}
